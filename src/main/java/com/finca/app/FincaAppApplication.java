5e0c37aca9a34bc6187506f2fa355a6f68c28f31
maikffDev <211547847+maikffDev@users.noreply.github.com.>
2025-06-24 10:32:32 -0300

    primer commit

---
+++ src/main/java/com/finca/app/FincaAppApplication.java
@@ -0,0 +1,13 @@
+package com.finca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaAppApplication.class, args);
+	}
+
+}
