afe1ce36f70aeeba63d39402f6807b4b3f2925b5
matucsom <sn.matias@yahoo.com.ar>
2025-06-26 14:22:15 -0300

    feat(Entity) agregando UserEntity

--- src/main/java/com/finca/app/insfraestructure/entities/UserEntity.java
+++ src/main/java/com/finca/app/insfraestructure/entities/UserEntity.java
@@ -1,4 +1,34 @@
 package com.finca.app.insfraestructure.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique=true,nullable=false)
+    private String nombre;
+
+    @Column(unique = true,nullable=false)
+    private String correo;
+
+    @Column(nullable=false)
+    private String contrasena;
+
+    private int intFallidosIngreso;
+
+    private boolean activo;
+
+    @ManyToOne
+    @JoinColumn(name = "rol_id")
+    private RoleEntity rolId;
+
 }
