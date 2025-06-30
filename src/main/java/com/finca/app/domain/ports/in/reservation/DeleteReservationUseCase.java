f7baacdf90e382e7c51f38d082602cabc2f3dfd2
matucson1 <115057949+matuteFanDeWandaNara@users.noreply.github.com>
2025-06-30 13:34:23 -0300

    corrigiendo errores de traduccion

--- src/main/java/com/finca/app/domain/ports/in/reservation/DeleteReservationUseCase.java
+++ src/main/java/com/finca/app/domain/ports/in/reservation/DeleteReservationUseCase.java
@@ -1,9 +1,8 @@
-package com.finca.app.domain.ports.in.reservation;
+package com.finca.app.domain.ports.in.Reservation;
 
 import com.finca.app.domain.models.Reservation;
 
 public interface DeleteReservationUseCase {
-
     Boolean delete(Long id);
 
     Reservation logicalDeletion( Reservation reservation);
