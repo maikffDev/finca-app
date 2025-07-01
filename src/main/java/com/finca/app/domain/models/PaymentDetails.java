51f29e5d4f7a7ee6b09ba65583d04367a4a04598
matucsom <sn.matias@yahoo.com.ar>
2025-07-01 11:16:34 -0300

    feat(domain)(models) caambios en Modelos, agregacion y fix para que se ajuste al nuevo DER

---
+++ src/main/java/com/finca/app/domain/models/PaymentDetails.java
@@ -0,0 +1,9 @@
+package com.finca.app.domain.models;
+
+public class PaymentDetails {
+
+    private Long id;
+    private User user;
+    private int CBU;
+    private String alias;
+}
--- src/main/java/com/finca/app/domain/models/User.java
+++ src/main/java/com/finca/app/domain/models/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,5 +16,6 @@ public class User {
     private String contrasena;
     private int intFallidosIngreso;
     private Boolean activo;
-    private Boolean rollId;
+    private List<Role>roles;
+    private List<PaymentDetails> paymentDetailsList;
 }
