4d8decb19e5677d1b8cfd3f2a00e91a837c48742
matucson1 <115057949+matuteFanDeWandaNara@users.noreply.github.com>
2025-07-14 12:07:43 -0300

    fix()corrigiendo CORS ubicacion

--- src/main/java/com/finca/app/infrastructure/config/WebConfig.java
+++ src/main/java/com/finca/app/infrastructure/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.finca.app.application.config;
+package com.finca.app.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
