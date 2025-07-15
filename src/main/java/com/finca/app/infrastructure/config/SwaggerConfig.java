548b5abee8aa2f1a898dabbc0d7409d0020a57ac
FigueroaMauro <maurodev.tech@gmail.com>
2025-07-14 22:51:08 -0300

    feat(Swagger): se agregó la configuración basica de swagger

---
+++ src/main/java/com/finca/app/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.finca.app.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Finca App")
+                        .version("1.0")
+                        .description("Documentación de la aplicación de Reserva de Fincas"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
